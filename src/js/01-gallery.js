22715cc03a1c5769a3d4541dc2143a9b2cd7e7d9
EwaMazurek <ewa.mmazurek@gmail.com>
2023-01-24 12:07:41 +0100

    task 1 complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+galleryItems.forEach(img => {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__item" href=${img.original}>
+<img class="gallery__image" src=${img.preview} alt=${img.description} />
+</a>`
+  );
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
 console.log(galleryItems);
